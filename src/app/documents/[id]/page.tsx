2a625f3b78a0889e0faed29792aa70741224938e
Tommy Grabowski <60988025+tomagrab@users.noreply.github.com>
2025-01-02 17:46:29 -0500

    Update DocumentPage component to include searchParams in props for enhanced functionality

--- src/app/documents/[id]/page.tsx
+++ src/app/documents/[id]/page.tsx
@@ -1,14 +1,14 @@
 import DocumentCard from '@/components/layout/documents/document-card/document-card';
 import { getDocumentById } from '@/lib/db/tables/documents/documents';
 
-interface DocumentPageProps {
-  params: {
-    id: string;
-  };
-}
+type Props = {
+  params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
 
-export default async function DocumentPage({ params }: DocumentPageProps) {
+export default async function DocumentPage({ params, searchParams }: Props) {
   const { id } = params;
+  console.log(searchParams);
 
   if (!id) {
     return <div>Document not found</div>;
